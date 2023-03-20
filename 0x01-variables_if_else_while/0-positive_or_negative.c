abf27aeae180949bf32f10b8daf47bcd1513c001
khaoulaHamdouch <khaoulahamdouch01@gmail.com>
2023-03-20 10:40:08 -0700

    task0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/** 
+/**
   *main - Entry point
   *Discription: 'check for number if it is positive or negative'
-  *return: always 0
+  *Return: always 0
   */
 int main(void)
 {

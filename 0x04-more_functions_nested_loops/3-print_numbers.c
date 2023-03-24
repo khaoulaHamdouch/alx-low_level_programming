ca0fbb9331eb2f2bfdea3633bcc94348b66fa9b7
khaoulaHamdouch <khaoulahamdouch01@gmail.com>
2023-03-23 18:08:20 -0700

    3 print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  *_putchar - prints the numbers from 0 to 9
+  *followed by a new line
+  *
+  *Return: 0
+  */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}

4f9cd05eef476ea185bf087ca6334463da4b27cc
khaoulaHamdouch <khaoulahamdouch01@gmail.com>
2023-03-23 19:01:26 -0700

    5 10times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  *more_numbers - prints 10 times numbers from 0 to 14
+  *
+  *Return: 0
+  */
+void more_numbers(void)
+{
+	int n;
+	int m;
+
+	for (n = 1; n <= 10; n++)
+	{
+		for (m = 0; m <= 14; m++)
+		{
+			if (m >= 10)
+				_putchar('1');
+			_putchar (j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}

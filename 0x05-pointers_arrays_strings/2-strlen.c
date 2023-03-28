e20fcde306437eeadc78e91294f3fa8003dee2fc
khaoulaHamdouch <khaoulahamdouch01@gmail.com>
2023-03-27 18:05:57 -0700

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  *int_strlen - return lenght of a string
+  *@s: string
+  *Return: lenght of string
+  */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	
+		length++;
+
+	return(length);
+}

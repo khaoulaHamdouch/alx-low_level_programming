3778d26aaf8ecbbc6df8cacc675f1f45dcd8f1bf
khaoulaHamdouch <khaoulahamdouch01@gmail.com>
2023-03-27 18:32:27 -0700

    task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  *print_rev - prints a string in reverse
+  *@s: string
+  *
+  *Return: 0
+  */
+void print_rev(char *s)
+{
+	int length = 0, m;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+
+	for (m = length; m > 0; m--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}

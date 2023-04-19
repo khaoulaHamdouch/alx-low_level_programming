86bacaa127b6fdcc7cbbdde133acdc2ba272613b
khaoulaHamduch <khaoulahamdouch01@gmail.com>
2023-04-18 18:11:27 -0700

    1

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,11 +1,4 @@
 #ifndef PI_H
 #define PI_H
-
-/**
-  *Desc: a header file that defines a macro named
-  *PI as an abbreviation for the token 3.14159265359
-  *
-  */
 #define PI 3.14159265359
-
 #endif

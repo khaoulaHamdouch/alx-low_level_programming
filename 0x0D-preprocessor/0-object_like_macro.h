d4007121d7b80229196040e1f104f160882ea35a
khaoulaHamduch <khaoulahamdouch01@gmail.com>
2023-04-18 18:09:09 -0700

    0

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,12 +1,4 @@
 #ifndef OBJECT_LIKE_MACRO_H
 #define OBJECT_LIKE_MACRO_H
-
-/**
-  *Desc: a header file that defines a macro
-  *		named SIZE as an abbreviation for
-  *		token 1024
-  */
-
 #define SIZE 1024
-
 #endif

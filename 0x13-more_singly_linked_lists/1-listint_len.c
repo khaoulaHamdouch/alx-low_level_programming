3fb0109d8d7d97566e023d81d28261514b20cb80
khaoulaHamdouch <khaoulahamdouch01@gmail.com>
2023-04-29 15:08:11 -0700

    fiix

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -5,7 +5,7 @@
  * a linked listint_t list
  * @h: a pointer to the head of the listint_t list
  * Return: the number of elements in the listint_t list
-*/
+ */
 size_t listint_len(const listint_t *h)
 {
     size_t num_nodes = 0;

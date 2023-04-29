3376829980cdf00de3aeea59af495620fbefdaa7
khaoulaHamdouch <khaoulahamdouch01@gmail.com>
2023-04-29 15:04:06 -0700

    betty

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,13 +7,13 @@
 */
 size_t print_listint(const listint_t *h)
 {
-    size_t num_nodes = 0;
+	size_t num_nodes = 0;
 
-    while (h)
-    {
-        num_nodes++;
-        printf("%d\n", h->next);
-        h = h->next;
-    }
-    return (num_nodes);
+	while (h)
+	{
+		num_nodes++;
+		printf("%d\n", h->next);
+		h = h->next;
+	}
+	return (num_nodes);
 }
